33e68a1c06dda653a49d1493f737b701bacfa89a
Ethan Olsen <131eolsen@gmail.com>
2021-02-05 14:28:32 -0800

    favicon / deleted unneeded images

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,10 +24,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#101319`,
+        theme_color: `#101319`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/eolsen-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
