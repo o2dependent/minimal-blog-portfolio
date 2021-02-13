32290f0b295ce7f1e8cb83428da9c8977b89c5cf
Ethan Olsen <131eolsen@gmail.com>
2021-02-12 19:09:46 -0800

    tailwind auto post display to use auto-fill

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         108: "27rem",
       },
       gridTemplateColumns: {
-        "auto-post-display": "repeat(auto-fit, minmax(475px, 1fr))",
+        "auto-post-display": "repeat(auto-fill, minmax(475px, 1fr))",
         "post-display": "repeat(3, minmax(475px, 1fr))",
         "post-display-mobile": "repeat(3, minmax(300px, 1fr))",
       },
